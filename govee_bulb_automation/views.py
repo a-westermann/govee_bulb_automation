aa79ed896fc3efe80ba70873bdfa69e8d94723ab
Vierce1 <a.westermann.19@gmail.com>
2025-05-23 11:34:08 -0500

    payloads

--- govee_bulb_automation/views.py
+++ govee_bulb_automation/views.py
@@ -17,33 +17,35 @@ from .payloads import *
 logger = logging.getLogger('govee_bulb_automation')
 
 def bulb_home(request):
-    devices = get_devices()
+    # devices = get_devices()
+    devices = []
     context = {'devices':devices}
     return render(request, 'govee_bulb_automation/bulb_home.html',
                   context=context)
 
-# @csrf_exempt
-# def toggle_light(request):
-#     devices = get_devices()
-#     data = json.loads(request.body)
-#     state = data.get('state')
-#     logger.log(level=10, msg=f'toggle light. Devices: {len(devices)}')
-#     response = None
-#
-#     for device in devices:
-#         payload = get_toggle_light(device.device_id, device.model, state)
-#         response = requests.post('https://developer-api.govee.com/v1/devices/control', data=json.dumps(payload),
-#                                  headers={'Accept': 'application/json','Govee-API-Key': API_KEY})
-#         logger.log(level=10, msg=response.content)
-#         return JsonResponse({'success': True})
-#     if response:
-#         decoded = response.json()
-#         toggle_light_response = {
-#             'status_code': response.status_code,
-#             'response': decoded
-#         }
-#         return JsonResponse({'success': True, 'response': toggle_light_response})
-#     else:
-#         toggle_light_response = {}
-#         return JsonResponse({'success': False, 'response': toggle_light_response})
+@csrf_exempt
+def toggle_light(request):
+    # devices = get_devices()
+    devices = []
+    data = json.loads(request.body)
+    state = data.get('state')
+    logger.log(level=10, msg=f'toggle light. Devices: {len(devices)}')
+    response = None
+
+    for device in devices:
+        payload = get_toggle_light(device.device_id, device.model, state)
+        response = requests.post('https://developer-api.govee.com/v1/devices/control', data=json.dumps(payload),
+                                 headers={'Accept': 'application/json','Govee-API-Key': API_KEY})
+        logger.log(level=10, msg=response.content)
+        return JsonResponse({'success': True})
+    if response:
+        decoded = response.json()
+        toggle_light_response = {
+            'status_code': response.status_code,
+            'response': decoded
+        }
+        return JsonResponse({'success': True, 'response': toggle_light_response})
+    else:
+        toggle_light_response = {}
+        return JsonResponse({'success': False, 'response': toggle_light_response})
 
