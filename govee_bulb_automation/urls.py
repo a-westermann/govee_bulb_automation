ab4a4692ef02bb54458479b2cf0adb04e5569696
Vierce1 <a.westermann.19@gmail.com>
2025-05-27 10:48:58 -0500

    temp set

--- govee_bulb_automation/urls.py
+++ govee_bulb_automation/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('bulb_home/', bulb_home, name='bulb_home'),
     path('toggle_light/', toggle_light, name='toggle_light'),
+    path('set_temperature/', set_temperature, name='set_temperature'),
 ]
