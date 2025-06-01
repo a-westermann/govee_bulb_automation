10a027c9cb504b8a56f72e7e552fc1a1bfc5a9a8
Andrew <a.westermann.19@gmail.com>
2025-06-01 16:48:45 -0500

    auto mode background loop

--- govee_bulb_automation/views.py
+++ govee_bulb_automation/views.py
@@ -106,10 +106,10 @@ def auto_loop():
                 data=json.dumps({'brightness': brightness}),
                 headers={'Content-Type': 'application/json'}
             )
-            print("auto success")
+            logger.debug("auto success")
             time.sleep(60)
         except Exception as e:
-            print(f"Failed auto. {e}")
+            logger.debug(f"Failed auto. {e}")
 
 
 
