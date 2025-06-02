e959190a2acebaaa515211174551f8312e3f412b
Vierce1 <a.westermann.19@gmail.com>
2025-06-02 13:00:11 -0500

    auto mode changes

--- govee_bulb_automation/weather.py
+++ govee_bulb_automation/weather.py
@@ -24,38 +24,45 @@ def get_color_from_condition(condition_main, condition_desc):
 def calculate_light_temperature(sunrise_dt, sunset_dt, current_dt=None, min_temp=2700, max_temp=6500):
     now = current_dt or datetime.utcnow()
 
-    sunrise_end = sunrise_dt + timedelta(hours=2)
-    sunset_start = sunset_dt - timedelta(hours=2)
+    sunrise_start = sunrise_dt - timedelta(minutes=30)
+    sunrise_end = sunrise_dt + timedelta(hours=3)
 
-    if now < sunrise_dt:
+    sunset_start = sunset_dt - timedelta(hours=3)
+    sunset_end = sunset_dt - timedelta(minutes=30)
+
+    if now < sunrise_start:
         return min_temp
-    elif sunrise_dt <= now < sunrise_end:
-        ratio = (now - sunrise_dt).total_seconds() / (sunrise_end - sunrise_dt).total_seconds()
+    elif sunrise_start <= now < sunrise_end:
+        ratio = (now - sunrise_start).total_seconds() / (sunrise_end - sunrise_start).total_seconds()
         return int(min_temp + (max_temp - min_temp) * ratio)
     elif sunrise_end <= now < sunset_start:
         return max_temp
-    elif sunset_start <= now < sunset_dt:
-        ratio = (now - sunset_start).total_seconds() / (sunset_dt - sunset_start).total_seconds()
+    elif sunset_start <= now < sunset_end:
+        ratio = (now - sunset_start).total_seconds() / (sunset_end - sunset_start).total_seconds()
         return int(max_temp - (max_temp - min_temp) * ratio)
     else:
         return min_temp
 
 
+
 def calculate_brightness(sunrise_dt, sunset_dt, current_dt=None, min_brightness=5, max_brightness=75):
     now = current_dt or datetime.utcnow()
 
-    sunrise_end = sunrise_dt + timedelta(hours=2)
-    sunset_start = sunset_dt - timedelta(hours=2)
+    sunrise_start = sunrise_dt - timedelta(minutes=30)
+    sunrise_end = sunrise_dt + timedelta(hours=3)
 
-    if now < sunrise_dt:
+    sunset_start = sunset_dt - timedelta(hours=3)
+    sunset_end = sunset_dt - timedelta(minutes=30)
+
+    if now < sunrise_start:
         return min_brightness
-    elif sunrise_dt <= now < sunrise_end:
-        ratio = (now - sunrise_dt).total_seconds() / (sunrise_end - sunrise_dt).total_seconds()
+    elif sunrise_start <= now < sunrise_end:
+        ratio = (now - sunrise_start).total_seconds() / (sunrise_end - sunrise_start).total_seconds()
         return round(min_brightness + (max_brightness - min_brightness) * ratio)
     elif sunrise_end <= now < sunset_start:
         return max_brightness
-    elif sunset_start <= now < sunset_dt:
-        ratio = (now - sunset_start).total_seconds() / (sunset_dt - sunset_start).total_seconds()
+    elif sunset_start <= now < sunset_end:
+        ratio = (now - sunset_start).total_seconds() / (sunset_end - sunset_start).total_seconds()
         return round(max_brightness - (max_brightness - min_brightness) * ratio)
     else:
         return min_brightness
